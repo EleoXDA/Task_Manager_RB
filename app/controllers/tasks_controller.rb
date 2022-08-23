b11a561a1cda3b535a88e094ce241a2c12ea6489
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-23 16:06:30 +0200

    ADD: controller for destroying task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,6 +28,12 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def task_params
