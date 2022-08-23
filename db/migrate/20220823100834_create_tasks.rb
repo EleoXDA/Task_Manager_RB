b75e14eb5ceabed55e660e2e7cfd6215b00d8400
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-23 12:23:38 +0200

    EDIT: add default value for completed

--- db/migrate/20220823100834_create_tasks.rb
+++ db/migrate/20220823100834_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[7.0]
     create_table :tasks do |t|
       t.string :title
       t.text :details
-      t.boolean :completed
+      t.boolean :completed, default: false
 
       t.timestamps
     end
