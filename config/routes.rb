09a6470ccc2b5d159d71fcf8d514b8896c24fd40
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-23 16:01:25 +0200

    FIX: missing ":"

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
   patch 'tasks/:id', to: 'tasks#update'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: edit_task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
 end
