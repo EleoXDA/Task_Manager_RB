acaa06e3172713408a51921c4a8b93eceaadb04c
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-23 14:37:23 +0200

    DB update

--- db/migrate/20220823100834_create_tasks.rb
+++ db/migrate/20220823100834_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.string :title
-      t.text :details
+      t.string :title, null: false
+      t.text :details, null: false
       t.boolean :completed, default: false
 
       t.timestamps
