f4db30d6e35bbaa3a7f800db196cc2737f0d8c74
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-23 15:05:32 +0200

    ADD: routes for "create task"

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: 'tasks#index'
   get 'tasks', to: 'tasks#index', as: :tasks
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
 end
