7390ad3acf9f114c4c68fdbf660c97c172e1c491
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-23 15:49:38 +0200

    ADD: create function with required permissions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,16 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details)
+  end
 end
