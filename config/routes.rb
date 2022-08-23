5968e48c7dea3501dd065013b9a9cc7345bf2454
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-23 15:53:18 +0200

    ADD: routes for updating task

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   post 'tasks', to: 'tasks#create'
   get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: edit_task
 end
