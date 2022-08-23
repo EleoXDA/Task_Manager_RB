64e0c451cf7023e3ad6fa96ccd056ec2ccdb0fa2
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-23 16:06:17 +0200

    ADD: route for destroying task

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   get 'tasks/:id', to: 'tasks#show', as: :task
   patch 'tasks/:id', to: 'tasks#update'
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
