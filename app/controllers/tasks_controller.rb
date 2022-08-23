6bf7196d7659400d270756abea13562d786e2b61
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-23 15:55:52 +0200

    ADD: controller for update task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,14 +13,24 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(tasks_params)
+    @task = Task.new(task_params)
     @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   private
 
-  def tasks_params
+  def task_params
     params.require(:task).permit(:title, :details)
   end
 end
