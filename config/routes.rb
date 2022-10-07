447662d9ca11fb29dbfd031cc030cd746b7f990c
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-07 23:39:48 +0200

    update routes sequences

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post 'tasks', to: 'tasks#create'
   get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
-  patch 'tasks/:id', to: 'tasks#update'
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
   delete 'tasks/:id', to: 'tasks#destroy'
 end
